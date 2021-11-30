c7d9fb610bfd131160eb348741d3c76b989d12ff
dingwei <602017206@qq.com>
2021-11-30 16:15:29 +0800

    111

--- src/main/java/org/sample/MainApplication.java
+++ src/main/java/org/sample/MainApplication.java
@@ -1,17 +1,32 @@
 package org.sample;
 
+import com.alibaba.nacos.api.annotation.NacosInjected;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingService;
 import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
+
 @Configuration
 @SpringBootApplication(scanBasePackages = "org.sample")
 @NacosPropertySource(dataId = "sample-site", autoRefreshed = true)
 public class MainApplication {
+
+	@NacosInjected
+	private NamingService namingService;
+
+	@Value("${spring.application.name}")
+	private String applicationName;
+
+	@Value("${server.port}")
+	private Integer serverPort;
 
 	static Logger logger = LoggerFactory.getLogger(MainApplication.class);
 
@@ -21,6 +36,11 @@ public class MainApplication {
 	public static void main(String[] args) {
 	    SpringApplication.run(MainApplication.class, args);
 	    logger.info(" ------------ start success -----------");
+	}
+
+	@PostConstruct
+	public void registerService() throws NacosException {
+		namingService.registerInstance(applicationName, "82.157.129.159", serverPort);
 	}
 
 }
