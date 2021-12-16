61c4ec8fbaf11277edd9dd69ef13d79ac522f59c
dingwei <602017206@qq.com>
2021-12-16 15:25:59 +0800

    定时刷新

--- src/main/java/org/sample/web/AccessToken.java
+++ src/main/java/org/sample/web/AccessToken.java
@@ -1,10 +1,13 @@
 package org.sample.web;
 
+import org.sample.config.SampleSiteProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.Resource;
 import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
@@ -13,6 +16,9 @@ public class AccessToken implements CommandLineRunner {
     private static Logger logger = LoggerFactory.getLogger(AccessToken.class);
 
 
+    @Resource
+    private SampleSiteProperties sampleSiteProperties = new SampleSiteProperties();
+
     @Override
     public void run(String... args) throws Exception {
 
@@ -20,6 +26,7 @@ public class AccessToken implements CommandLineRunner {
             try {
                 String accessTokenDO = "";
                 if (accessTokenDO != null) {
+                    sampleSiteProperties.setProp(accessTokenDO);
                     logger.info("accessToken : " + accessTokenDO);
                     TimeUnit.SECONDS.sleep(7000);
                 } else {
