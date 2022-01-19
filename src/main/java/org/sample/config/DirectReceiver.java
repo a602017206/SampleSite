7a3f1258039611c2a7e9d3740b3dcd6765fe9d3b
dingwei <602017206@qq.com>
2022-01-19 11:00:53 +0800

    rabbitMq添加

---
+++ src/main/java/org/sample/config/DirectReceiver.java
@@ -0,0 +1,24 @@
+package org.sample.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * @author
+ * RabbitMq消息监听
+ */
+@Component
+@RabbitListener(queues = "TestDirectQueue")//监听的队列名称 TestDirectQueue
+@Log4j2
+public class DirectReceiver {
+
+    @RabbitHandler
+    public void process(Map testMessage) {
+        log.info("DirectReceiver消费者收到消息  : " + testMessage.toString());
+    }
+
+}
---
+++ src/main/java/org/sample/web/SendMessageController.java
@@ -0,0 +1,42 @@
+package org.sample.web;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * @author
+ * @deprecated
+ * @desc rabbitMq发送消息测试
+ */
+@RestController
+@RequestMapping(value = "/rabbitMq")
+public class SendMessageController {
+
+    //使用RabbitTemplate,这提供了接收/发送等等方法
+    @Resource
+    RabbitTemplate rabbitTemplate;
+
+    @GetMapping("/sendDirectMessage")
+    public String sendDirectMessage() {
+        String messageId = String.valueOf(UUID.randomUUID());
+        String messageData = "test message, hello!";
+        String createTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        Map<String,Object> map=new HashMap<>();
+        map.put("messageId",messageId);
+        map.put("messageData",messageData);
+        map.put("createTime",createTime);
+        //将消息携带绑定键值：TestDirectRouting 发送到交换机TestDirectExchange
+        rabbitTemplate.convertAndSend("TestDirectExchange", "TestDirectRouting", map);
+        return "ok";
+    }
+
+}
