f4c94d706719e6da7f1cc6bd1200d627182e4326
dingwei <602017206@qq.com>
2022-03-04 18:00:55 +0800

    集成mybatis-plus

---
+++ src/main/java/org/sample/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package org.sample.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.sample.domain.User;
+
+/**
+* @author dingwei
+* @description 针对表【user(用户信息)】的数据库操作Mapper
+* @createDate 2022-03-04 17:59:33
+* @Entity org.sample.domain.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
