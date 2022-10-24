b49f49068d9804824a40487d3c82a64fb338b72f
dingwei <602017206@qq.com>
2022-10-24 10:13:05 +0800

    清除测试代码

--- src/main/java/org/sample/service/ElasticSerachService.java
+++
@@ -1,4 +0,0 @@
-package org.sample.service;
-
-public interface ElasticSerachService {
-}
--- src/main/java/org/sample/web/HelloController.java
+++ src/main/java/org/sample/web/HelloController.java
@@ -3,18 +3,14 @@ package org.sample.web;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.log4j.Log4j2;
-import org.apache.http.entity.ContentType;
 import org.sample.config.SampleSiteProperties;
 import org.sample.domain.User;
 import org.sample.service.UserService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -67,57 +63,6 @@ public class HelloController {
         return properties.toString();
     }
 
-    @GetMapping("/createFile")
-    public void createFile(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
-        String a = "qweqweqew";
-        try {
-            ServletOutputStream outputStream = httpServletResponse.getOutputStream();
-            httpServletResponse.setHeader("Content-Disposition", "attachment;filename=html.txt");
-            // 响应类型,编码
-            httpServletResponse.setContentType(ContentType.APPLICATION_OCTET_STREAM.getMimeType());
-            outputStream.write(a.getBytes(StandardCharsets.UTF_8));
-            outputStream.flush();
-            outputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
-    }
-
-    @PostMapping("/cqHttp")
-    public void cqHttp(@RequestBody Map<String, Object> map){
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            log.info(objectMapper.writeValueAsString(map));
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void main(String[] args) {
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        String a = "\\d{4}[-]\\d{2}[-]\\d{2}T";
-
-        Map<String, String> map = new HashMap<>();
-        map.put("x", a);
-
-        System.out.println(map.get("x"));
-
-        try {
-            String s = objectMapper.writeValueAsString(map);
-
-            System.out.println(s);
-
-            Map map1 = objectMapper.readValue(s, Map.class);
-
-            System.out.println(map1);
-
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println();
-    }
 
 }
